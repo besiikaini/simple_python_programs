e9fa4c2221f1fb57326761f8c59a191e8f43694d
besiikaini <53264246+besiikaini@users.noreply.github.com>
2019-08-09 00:15:41 +0530

    Add files via upload

---
+++ mathematical_function.py
@@ -0,0 +1,11 @@
+from math import *
+print(" SOME COMMONLY USED MATHEMATICAL FUNCTION")
+print(max(9,3)) # It finds the maximum between the two given numbers
+print(min(9,3)) # It finds the minimum between the two given numbers
+print(pow(4,6)) # It performs the  power operation on the given number
+print(round(4.6)) # Its rounds the given number
+print(sqrt(49)) # Its finds the square root of the given number
+
+
+
+
